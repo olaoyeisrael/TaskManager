2c32d1281b12b64fba6619ef4e6a489409d5a822
Israel <olaoyeaisrael@gmail.com>
2024-10-06 08:27:12 +0100

    initial commit

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,15 +6,5 @@ mongoose.connect(process.env.MONGODB_URL, {
 
 
 
-// const me = new User({
-//     name: '   Andrew  ',
-//     email: 'MYEMAIL@MEAD.IO   ',
-//     password: 'phone098!'
-// })
 
-// me.save().then(() => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log('Error!', error)
-// })
 
